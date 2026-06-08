7b549998f7f08e6657c7314afaf5141550c3b2da
Luiz Henrique <123130503+luizhcunha@users.noreply.github.com>
2026-06-08 09:31:13 +0000

    add services scripts

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,7 +3,7 @@ import database from "infra/database.js";
 async function status(request, response) {
   const query = await database.query("SELECT 1 + 1 AS sum;");
   console.log(query.rows);
-
+  console.log("Teste")
   response.status(200).json({ chave: "valor" });
 }
 
